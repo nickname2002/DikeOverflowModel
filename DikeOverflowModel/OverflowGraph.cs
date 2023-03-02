4b3e41c618dd787ca775e26552d92c730736cb90
Nick Jordan <nick@jorcademy.nl>
2023-03-02 17:48:38 +0100

    [FEATURE] Rewritten graph drawing, allowing us to implement exponential curves.

--- DikeOverflowModel/OverflowGraph.cs
+++ DikeOverflowModel/OverflowGraph.cs
@@ -66,39 +66,77 @@ public class OverflowGraph : Panel
         double dikeHeight = _settings.DikeHeight;
         double seaLevel = 1.04;
         double speedPerYear = _settings.RisingSpeed / 100;
-        int yearAmount = _settings.YearAmountGraph; 
+        double yearAmount = _settings.YearAmountGraph; 
         double minHeight = _settings.MinHeightGraph;
         double maxHeight = _settings.MaxHeightGraph;
         double factor = (HEIGHT / maxHeight);
 
         // NOTE: This implementation currently only support linear
-        
-        // Draw dike height
-        int dikeY = (int)(HEIGHT - (dikeHeight * factor) + (minHeight * factor));
-        gr.DrawLine(Pens.Green, new Point(0, dikeY), new Point(WIDTH, dikeY));        
-        
-        // Draw sea level
-        int y0 = (int)(HEIGHT - (seaLevel * factor) + (minHeight * factor));
-        int yEnd = (int)(HEIGHT - (seaLevel + (speedPerYear * yearAmount) * factor) + (minHeight * factor));
-        gr.DrawLine(Pens.Blue, new Point(0, y0), new Point(WIDTH, yEnd));
 
+        Point dikePrev = new Point();
+        Point waterPrev = new Point();
+        
+        for (int i = 0; i < WIDTH; i++)
+        {
+            if (i == 0)
+            {
+                dikePrev = new Point(i, (int)(HEIGHT - (dikeHeight * factor) + (minHeight * factor) + (minHeight * factor)));
+                waterPrev = new Point(i, (int)(HEIGHT - (seaLevel * factor) + (minHeight * factor)));
+                continue;
+            }
+            
+            Point newDike;
+            Point newWater;
+            
+            int dikeX = i;
+            int dikeY = (int)(HEIGHT - (dikeHeight * factor) + (minHeight * factor));
+
+            int waterX = i;
+            int waterY = (int)(HEIGHT - ((seaLevel + (speedPerYear * i * (yearAmount / WIDTH))) * factor) + (minHeight * factor));
+
+            // Create new points
+            newDike = new Point(dikeX, dikeY);
+            newWater = new Point(waterX, waterY);
+            
+            // Draw lines
+            gr.DrawLine(Pens.Green, dikePrev, newDike);
+            gr.DrawLine(Pens.Blue, waterPrev, newWater);
+            
+            // Set new prev points
+            dikePrev = newDike;
+            waterPrev = newWater;
+        }
+        
+        // Dike height
+        int dikeYStart = (int)(HEIGHT - (dikeHeight * factor) + (minHeight * factor));
+        
+        // Sea level
+        int waterYStart = (int)(HEIGHT - (seaLevel * factor) + (minHeight * factor));
+        int waterYEnd = (int)(HEIGHT - (seaLevel + (speedPerYear * yearAmount) * factor) + (minHeight * factor));
+        
         // Draw coordinates
         gr.DrawString(
             $"(0, {dikeHeight})", 
             new Font("Bahnschrift", 10), 
             Brushes.Green, 
-            new Point(5, dikeY - 20));
+            new Point(5, dikeYStart - 20));
+        
+        gr.DrawString(
+            $"({yearAmount}, {dikeHeight})", 
+            new Font("Bahnschrift", 10), 
+            Brushes.Green, 
+            new Point(WIDTH - 90, dikeYStart - 20));
         
         gr.DrawString(
             $"(0, {seaLevel})", 
             new Font("Bahnschrift", 10), 
             Brushes.Blue, 
-            new Point(5, y0 - 20));
+            new Point(5, waterYStart - 20));
 
         gr.DrawString(
             $"({yearAmount}, {seaLevel + (speedPerYear * yearAmount)})", 
             new Font("Bahnschrift", 10), 
             Brushes.Blue, 
-            new Point(WIDTH - 90, yEnd - 20));
+            new Point(WIDTH - 90, waterYEnd - 20));
     }
 }
