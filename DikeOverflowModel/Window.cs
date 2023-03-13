98a04dbd9e04cbb50d13f50efeaed87ca8d4870a
Nick Jordan <nick@jorcademy.nl>
2023-03-13 23:35:37 +0100

    [FEATURE] Add app icon.

--- DikeOverflowModel/Window.cs
+++ DikeOverflowModel/Window.cs
@@ -4,12 +4,14 @@ public class Window : Form
 {
     private SimulationView _simulationView;
     
+
     public Window()
     {
         this.ClientSize = new Size(1600, 930);
         this.Text = "DikeSim";
         this.DoubleBuffered = true;
         this._simulationView = new SimulationView();
+        this.Icon = Properties.Resources.dikesim_icon;
 
         // Disable resizable window
         this.MinimumSize = this.ClientSize;
